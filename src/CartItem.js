9b23257d179ea5bd85f0fe0fde0bab83105673e7
Pluang <pluang@Pluangs-MacBook-Pro.local>
2021-03-25 11:28:09 +0530

    setState in depth

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,28 +11,120 @@ class CartItem extends React.Component {
         }
         // this.increaseQuantity = this.increaseQuantity.bind(this);   
         //instead this use arrow functions as they automatically bind state
+
+        this.testing();
     }
 
+    
+    
+    // In case of AJAX call or in case of promise React donot perform batching
+    // Also in that case setState call is Synchronous
+    // testing() {
+    //     const promise = new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             resolve('done')
+    //         }, 5000);
+    //     })
+
+    //     promise.then(() => {
+    //         //In case of setState setState call is Synchronous
+    //         //this.setState({ qty: this.state.qty + 10});
+
+
+    //         //Here in case of promise our Component is re-rendered 3 times
+    //         this.setState({ qty: this.state.qty + 10});
+    //         this.setState({ qty: this.state.qty + 10});
+    //         this.setState({ qty: this.state.qty + 10});
+
+    //         console.log('state', this.state);
+    //     });
+    // }
+    
     increaseQuantity = () => {
         
         //console.log('this.state', this.state);
-
         //setState form 1
         //this state do shallow merging.......which means if I change only qty then only qty will change
         // this.setState({
         //     qty: this.state.qty + 1
         // }); 
 
+
+        //Even though I'm rendering 3 times, but React only renders it for single time. 
+        //This is because of the concept of batching.
+        //By batching I mean the event handler batch all the setStates as a single unit.
+        //Therefore these 3 setState calls merge into one batch i.e, one setState.
+
+        //In short, React will take only last call setState object. 
+        // this.setState({
+        //     qty: this.state.qty + 5
+        // }); 
+
+        // this.setState({
+        //     qty: this.state.qty + 6
+        // }); 
+
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // }); 
+
+
         //setState form 2
         //This also do shallow merging
+        // this.setState((prevState) => {
+        //     return {
+        //         qty: prevState.qty + 1
+        //     }
+        // });
+
+
+        //Here our quantity increases by 3 but React only render once i.e, here also React performing batching
+        //Check inside render func console.log('render') in console
+
+        //This 2nd form basically uses Queue and pass the setState callback to it
+        //Since here we have access to prevState so react ensure that prevState value should be updated
+        // this.setState((prevState) => {
+        //     return {
+        //         qty: prevState.qty + 1
+        //     }
+        // });
+
+          // this.setState((prevState) => {
+        //     return {
+        //         qty: prevState.qty + 1
+        //     }
+        // });
+        // this.setState((prevState) => {
+        //     return {
+        //         qty: prevState.qty + 1
+        //     }
+        // });
+
+        //setState call is basically Asynchronous call
+        //therefore we donot know when it will be finished
+        // this.setState((prevState) => {
+        //     return {
+        //         qty: prevState.qty + 1
+        //     }
+        // });
+        // console.log(this.state);
+
+        //this setState contains 2 callback
+        //here the 2nd callback will be called whenever 1st callback will be finished
+        //this 2nd callback we can pass in the first state also
         this.setState((prevState) => {
             return {
                 qty: prevState.qty + 1
             }
+        }, () => {
+            console.log(this.state);
         });
     }
-
     decreaseQuantity = () => {
+        const {qty} = this.state;
+        if(qty === 0) {
+            return;
+        }
         this.setState((prevState) => {
             return {
                 qty: prevState.qty - 1
@@ -40,6 +132,7 @@ class CartItem extends React.Component {
         });
     }
     render() {
+        console.log('render');
         const {price, title, qty} = this.state; 
         return (
             <div className="cart-item">
