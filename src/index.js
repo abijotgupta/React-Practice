53933e7328602b2ef62fc908a53e3b6cc1682ba5
Abijot Gupta <abijotgupta1999@gmail.com>
2021-05-16 01:07:58 +0530

    Firebase Added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "xxxxxxxxxxxxxxxx",
+  authDomain: "xxxx-xxxxx.xxxxx.xxxx",
+  projectId: "xxx-xxxxx",
+  storageBucket: "xxxx-xxxx.xxxxxxx.xxxx",
+  messagingSenderId: "xxxxxxxxxxxxxx",
+  appId: ":xxxxxxxxxxxxxxxxxx:"
+};
+// Initialize Firebasex
+firebase.initializeApp(firebaseConfig);
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
